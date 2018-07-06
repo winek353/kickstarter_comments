09fda80586403cee5df5f2fb4b18a3d6dd362b3b
jakub1024 <jakubxyz1@gmail.com>
2018-07-06 13:46:44 +0200

    wyświetlanie komentarzy

--- src/main/java/launcher/Main.java
+++ src/main/java/launcher/Main.java
@@ -7,13 +7,23 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import model.Comment;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import service.CommentsParserService;
+import service.CommentsService;
+import service.JsonCommentsWriter;
+import service.ServerConnectionService;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main extends Application {
 
@@ -33,21 +43,26 @@ public class Main extends Application {
         text.setText("1111");
         text2.setText("2222");
 
-        VBox vbox = (VBox) scene.lookup("#vbox");
-        vbox.getChildren().add(createAnchorPane());
-        vbox.getChildren().add(createAnchorPane());
-        vbox.getChildren().add(createAnchorPane());
-        for(int i=0;i<100000;i++){
+        AnchorPane mainPane = (AnchorPane) scene.lookup("#mainPane");
 
-            vbox.getChildren().add(createAnchorPane());
-        }
+        VBox vbox = (VBox) scene.lookup("#vbox");
+        ScrollPane scrollPane = (ScrollPane) scene.lookup("#scrollPane");
+        vbox.setSpacing(50);
+        showComments(vbox, scrollPane);
+        vbox.prefWidthProperty().bind(mainPane.widthProperty());
+        scrollPane.prefWidthProperty().bind(mainPane.widthProperty());
+        scrollPane.prefHeightProperty().bind(mainPane.heightProperty());
+//        vbox.getChildren().add(createAnchorPane());
+//        vbox.getChildren().add(createAnchorPane());
+//        vbox.getChildren().add(createAnchorPane());
+//        vbox.getChildren().add(createAnchorPane());
 
         primaryStage.show();
     }
-    public AnchorPane createAnchorPane(){
+    public AnchorPane createAnchorPane(Comment comment, ScrollPane scrollPane){
         AnchorPane anchorPane = new AnchorPane();
         anchorPane.prefHeight(100);
-        anchorPane.prefWidth(400);
+        // anchorPane.prefWidth(400);
 
         ImageView imageView = new ImageView();
         anchorPane.getChildren().add(imageView);
@@ -56,18 +71,46 @@ public class Main extends Application {
 
         Label label2 = createCommentTimeLabel();
 
-        Label label3 = createCommentLabel();
-
+        Label label3 = createCommentLabel(comment.getText());
+       // label3.prefWidthProperty().bind(vbox.widthProperty());
+        label3.prefWidthProperty().bind(scrollPane.widthProperty().subtract(100));
         anchorPane.getChildren().addAll(label, label2, label3);
 
         return anchorPane;
 
     }
 
+    private List<Comment> getAllComments(){
+        ApplicationContext ctx = new AnnotationConfigApplicationContext("service");
+        CommentsService commentsService =
+                (CommentsService) ctx.getBean("commentsService");
+        List<Comment> comments = null;
+        try {
+            comments = commentsService.getAllComments("https://www.kickstarter.com/projects/steamforged/critical-role");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("comments.size()");
+        System.out.println(comments.size());
+        System.out.println("comments.size()");
+        return comments;
+    }
+
+    private void showComments(VBox vbox, ScrollPane scrollPane){
+        List<Comment> comments = getAllComments();
+        comments.forEach(x->addComment(x,vbox, scrollPane));
+    }
+
+    private void addComment(Comment comment, VBox vbox, ScrollPane scrollPane){
+        AnchorPane a = createAnchorPane(comment, scrollPane);
+        vbox.getChildren().add(a);
+    }
+
     public Label createUsernameLAbel(){
         Label label = new Label();
         label.setLayoutX(45);
-        label.setLayoutY(20);
+        label.setLayoutY(28);
         label.setPrefHeight(15);
         label.setPrefWidth(150);
         label.setText("aaaaaa123");
@@ -78,22 +121,21 @@ public class Main extends Application {
         Label label = new Label();
         label.setText("3 hours ago");
         label.setLayoutX(160);
-        label.setLayoutY(20);
+        label.setLayoutY(28);
         label.setPrefHeight(15);
         label.setPrefWidth(80);
         return label;
     }
 
-    public  Label createCommentLabel(){
+    public  Label createCommentLabel(String text){
         Label label = new Label();
         label.setAlignment(Pos.TOP_LEFT);
-        label.setText("sdfsdfsdf");
+        label.setText(text);
         label.setLayoutX(45);
         label.setLayoutY(70);
-        label.setPrefHeight(200);
-        label.setPrefWidth(200);
+        label.setPrefWidth(400);
         label.setWrapText(true);
-        label.setText("777sdfsdfsfdn  ffdtddh4dh hd54hd54d54dh45d hg54hgd45hdg5gh3 5d53dg54gdg3 3g54dhghsgn54dfsd");
+        //label.setText("777sdfsdfsfdn  ffdtddh4dh hd54hd54d54dh45d hg54hgd45hdg5gh3 5d53dg54gdg3 3g54dhghsgn54dfsd");
         return label;
     }
 
